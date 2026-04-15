2280c9ebcdb67b2a56e4241a2a8857c6c9b12e5a
jl9904 <lopezjaden004@gmail.com>
2026-04-15 14:42:53 -0500

    added lighthouse detail view and route

--- controllers/lighthouse.js
+++ controllers/lighthouse.js
@@ -90,3 +90,17 @@ exports.lighthouse_view_all_Page = async function(req, res) {
         res.send(`{"error": ${err}}`);
     }
 };
+
+// Handle a show one view with id specified by query
+exports.lighthouse_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try {
+        result = await Lighthouse.findById(req.query.id)
+        res.render('lighthousedetail', 
+            { title: 'Lighthouse Detail', toShow: result });
+    }
+    catch(err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
--- routes/lighthouse.js
+++ routes/lighthouse.js
@@ -5,5 +5,8 @@ var router = express.Router();
 /* GET lighthouses page. */
 router.get('/', lighthouse_controllers.lighthouse_view_all_Page);
 
+/* GET detail lighthouse page */
+router.get('/detail', lighthouse_controllers.lighthouse_view_one_Page);
+
 module.exports = router;
 
