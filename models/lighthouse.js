96cf19e95b3150835bca949826b0b16b7c192bc9
jl9904 <lopezjaden004@gmail.com>
2026-04-22 17:08:03 -0500

    adjusted page to add login and register, only have access if you are logged in

--- models/lighthouse.js
+++ models/lighthouse.js
@@ -1,9 +1,23 @@
 const mongoose = require("mongoose");
 
 const lighthouseSchema = mongoose.Schema({
-    name: String,
-    location: String,
-    height: Number
+    name: {
+        type: String,
+        required: [true, "Lighthouse name is required"],
+        // BONUS: added name min and max requirement
+        minLength: [5, "Name must be at least 5 characters"],
+        maxLength: [30, "Name cannot exceed 30 characters"]
+    },
+    location: {
+        type: String,
+        // BONUS: added a requirement for a location
+        required: [true, "Location is required"]
+    },
+    height: {
+        type: Number,
+        min: [0, "Height must be at least 0"],
+        max: [1000, "Height cannot exceed 1000 feet"]
+    }
 });
 
 module.exports = mongoose.model("Lighthouse", lighthouseSchema);
--- routes/lighthouse.js
+++ routes/lighthouse.js
@@ -2,6 +2,15 @@ var express = require('express');
 const lighthouse_controllers = require('../controllers/lighthouse');
 var router = express.Router();
 
+// A little function to check if we have an authorized user and continue on or
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user){
+        return next();
+    }
+    res.redirect("/login");
+}
+
 /* GET lighthouses page. */
 router.get('/', lighthouse_controllers.lighthouse_view_all_Page);
 
@@ -9,13 +18,13 @@ router.get('/', lighthouse_controllers.lighthouse_view_all_Page);
 router.get('/detail', lighthouse_controllers.lighthouse_view_one_Page);
 
 /* GET create lighthouse page */
-router.get('/create', lighthouse_controllers.lighthouse_create_Page);
+router.get('/create', secured, lighthouse_controllers.lighthouse_create_Page);
 
 /* GET update lighthouse page */
-router.get('/update', lighthouse_controllers.lighthouse_update_Page);
+router.get('/update', secured, lighthouse_controllers.lighthouse_update_Page);
 
 /* GET delete lighthouse page */
-router.get('/delete', lighthouse_controllers.lighthouse_delete_Page);
+router.get('/delete', secured, lighthouse_controllers.lighthouse_delete_Page);
 
 module.exports = router;
 
